1e786ea22ec6990331524f55c20678b37774cb23
Arul-A <akarul2100@gmail.com>
2024-07-16 19:08:52 +0530

    package

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // This may not be necessary
-    port: 5173,
-  },
-  // Optionally adjust build options if needed
-  build: {
-    // You can customize rollup options if necessary
-    rollupOptions: {
-      // Example of externalizing specific modules
-      external: ['react-icons/fa'],
-    },
-  },
 });
